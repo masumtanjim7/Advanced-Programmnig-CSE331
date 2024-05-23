eeed0eb3ca7838c271dbc8a3fb33856e743ca570
MasumTanjim7 <masumbillah19999@gmail.com>
2024-05-24 02:48:32 +0600

    Masum

---
+++ NetBeansProjects/JavaPckages/src/main/java/com/mycompany/javapckages/JavaPckages.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javapckages;
+
+/**
+ *
+ * @author masum
+ */
+public class JavaPckages {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
